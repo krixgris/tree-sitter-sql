d3c8990792ef2e1a1a07d12dddf76fc208b1158f
krixgris <krixgris@github.com>
2025-05-01 17:54:31 +0200

    åäöÅÄÖ works, and [][]* changed to []?

--- grammar.js
+++ grammar.js
@@ -3606,7 +3606,7 @@ module.exports = grammar({
       seq("`", $._identifier, "`"),
     ),
     _tsql_parameter: $ => seq('@', $._identifier),
-    _identifier: _ => /[a-zA-Z_åäöÅÄÖ][0-9a-zA-Z_åäöÅÄÖ]*/,
+    _identifier: _ => /[0-9a-zA-Z_åäöÅÄÖ]+/,
   }
 
 });
